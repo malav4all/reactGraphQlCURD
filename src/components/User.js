5fca31b68c4132af15c1d43dcd0ce777615f9dd0
Malav Naagar <malavnagar90@gmail.com>
2021-10-07 15:31:39 +0530

    add bootstrap table in grapql

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,10 @@ import {useQuery,gql} from '@apollo/client'
 import {useMutation} from '@apollo/client'
 import {Delete_Post} from '../Graphql/Mutations'
 import {FetchAllUser} from '../Graphql/Query'
+import * as ReactBootstrap from 'react-bootstrap'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import cellEditFactory from 'react-bootstrap-table2-editor';
 const  User = ()=> {
     const {error,loading,data} = useQuery(FetchAllUser)
     const [deletePost,{err}] = useMutation(Delete_Post)
@@ -11,7 +15,7 @@ const  User = ()=> {
         if (data) {
             setPost(data.getAllPost)
         }
-        // console.log(data)
+        console.log(data)
     },[data])
     const removePost = (id) =>{
         deletePost({
@@ -19,9 +23,10 @@ const  User = ()=> {
                 id:id
             }
         }).then((res)=>{
-            console.log(res)
+            // console.log(res)
             if(res.data.deletePost.success){
-                alert( `${res.data.deletePost.message}`
+                alert( `${res.data.deletePost.message}`,
+                // window.location.reload()
                 )
             }else{
                 alert('error')
@@ -31,22 +36,48 @@ const  User = ()=> {
         //     console.log(err)
         // }
     }
+    const formatterButton = (id) =>{
+        return(
+            <>
+                <button onClick={()=>{
+                    console.log()
+                    removePost(id)
+                }}>Delete</button>
+            </>
+        )
+    }
+    const columns = [
+        {
+            dataField: "id",
+            text: "Id",
+            hidden: true,
+            csvExport: false,
+        },
+        {
+            dataField:"name",
+            text:"Name",
+            headerStyle: { minWidth: "200px" },
+        },
+        {
+            dataField:"email",
+            text:"Email"
+        },
+        {
+            dataField: "operation",
+            text: "Action",
+            formatter:(id)=>formatterButton(id)
+        },
+    ]
     return (
         <div>
-            {
-                post.map((val)=>{
-                    return (
-                        <>
-                            <h1>{val.name}</h1>
-                            <button onClick={()=>{
-                                removePost(val.id)
-                                console.log(val.id)
-                                // window.location.reload()
-                            }}>Delete Post</button>
-                        </>
-                    )
-                })
-            }
+            <BootstrapTable
+              keyField="id"
+              data={post}
+              columns={columns}
+              pagination={paginationFactory()}
+              noDataIndication="Table is Empty"
+              cellEdit={ cellEditFactory({ mode: 'dbclick',blurToSave: true }) }
+            />
         </div>
     )
 }
